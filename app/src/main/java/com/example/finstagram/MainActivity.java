f61605dc4fec462a295bb749762de8714a27efb1
Carolyn Wang <carolynw@fb.com>
2022-06-14 15:16:18 -0700

    added step 4: Querying the Parse database

--- app/src/main/java/com/example/finstagram/MainActivity.java
+++ app/src/main/java/com/example/finstagram/MainActivity.java
@@ -10,7 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +32,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
         btnLogout = findViewById(R.id.btnLogout);
+        
+        queryPosts();
+        
 
         btnLogout.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -40,6 +53,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post: posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 
     private void goLoginActivity() {
--- app/src/main/java/com/example/finstagram/ParseApplication.java
+++ app/src/main/java/com/example/finstagram/ParseApplication.java
@@ -3,12 +3,17 @@ package com.example.finstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("1qlGMHvW1XtQZnmI39aQiFLKpRnAznajx7UCnGqU")
                 .clientKey("VtnBWRnqAXXCA04T0ivOBuR9jknMCZuYyoek5rV2")
