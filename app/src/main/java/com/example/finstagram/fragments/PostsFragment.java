a127ce685044008fb7079f3b0ef7818f8150116e
Carolyn Wang <carolynw@fb.com>
2022-06-17 14:17:32 -0700

    added infinite scroll and loading; but timeline only loading currentUser's posts

--- app/src/main/java/com/example/finstagram/fragments/PostsFragment.java
+++ app/src/main/java/com/example/finstagram/fragments/PostsFragment.java
@@ -16,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.finstagram.EndlessRecyclerViewScrollListener;
 import com.example.finstagram.Post;
 import com.example.finstagram.PostsAdapter;
 import com.example.finstagram.R;
@@ -33,11 +34,15 @@ import java.util.List;
  */
 public class PostsFragment extends Fragment {
     private static final String TAG = "PostsFragment";
+    private static final int POSTS_TO_LOAD = 5;
     private Context mContext;
     private RecyclerView rvPosts;
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
     private SwipeRefreshLayout swipeContainer;
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private int scrollCounter;
+
 
     public PostsFragment(){
 
@@ -57,6 +62,7 @@ public class PostsFragment extends Fragment {
 
         mContext = getContext(); // MainActivity
         rvPosts = view.findViewById(R.id.rvPosts);
+        scrollCounter = POSTS_TO_LOAD;
 
         allPosts = new ArrayList();
         adapter = new PostsAdapter(mContext, allPosts);
@@ -84,15 +90,40 @@ public class PostsFragment extends Fragment {
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext);
+        rvPosts.setLayoutManager(linearLayoutManager);
+            // Retain an instance so that you can call `resetState()` for fresh searches
+            scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+                @Override
+                public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                    // Triggered only when new data needs to be appended to the list
+                    // Add whatever code is needed to append new items to the bottom of the list
+                    loadNextDataFromApi(scrollCounter);
+                    scrollCounter = scrollCounter + POSTS_TO_LOAD;
+                }
+            };
+            // Adds the scroll listener to RecyclerView
+            rvPosts.addOnScrollListener(scrollListener);
+
     }
 
-    protected void queryPosts() {
-        // specify what type of data we want to query - Post.class
+    // Append the next page of data into the adapter
+    // This method probably sends out a network request and appends new data items to your adapter.
+    public void loadNextDataFromApi(int offset) {
+        // Send an API request to retrieve appropriate paginated data
+        //  --> Send the request including an offset value (i.e `page`) as a query parameter.
+        //  --> Deserialize and construct new model objects from the API response
+        //  --> Append the new data objects to the existing set of items inside the array of items
+        //  --> Notify the adapter of the new items made with `notifyItemRangeInserted()`
+
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
-        query.setLimit(20);
+        query.setLimit(POSTS_TO_LOAD);
         query.addDescendingOrder(Post.KEY_CREATED_AT);
-        // start an asynchronous call for posts
+        query.setSkip(offset);
+
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
@@ -112,5 +143,38 @@ public class PostsFragment extends Fragment {
                 adapter.notifyDataSetChanged();
             }
         });
+
     }
+
+    protected void queryPosts() {
+        loadNextDataFromApi(0);
+    }
+
+//    protected void queryPosts() {
+//        // specify what type of data we want to query - Post.class
+//        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+//        query.include(Post.KEY_USER);
+//        query.setLimit(POSTS_TO_LOAD);
+//        query.addDescendingOrder(Post.KEY_CREATED_AT);
+//        // start an asynchronous call for posts
+//        query.findInBackground(new FindCallback<Post>() {
+//            @Override
+//            public void done(List<Post> posts, ParseException e) {
+//                // check for errors
+//                if (e != null) {
+//                    Log.e(TAG, "Issue with getting posts", e);
+//                    return;
+//                }
+//
+//                // for debugging purposes let's print every post description to logcat
+//                for (Post post : posts) {
+//                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+//                }
+//
+//                // save received posts to list and notify adapter of new data
+//                allPosts.addAll(posts);
+//                adapter.notifyDataSetChanged();
+//            }
+//        });
+//    }
 }
